11a7be1fb11d211a1f04e7717a921857ebc4016e
Martin Madrigal <teenmv92@gmail.com>
2017-05-26 22:42:27 -0600

    My first IO Async

--- program.js
+++ program.js
@@ -1,4 +1,15 @@
 var fs = require('fs');
-//var buffer = fs.readFileSync(process.argv[2], 'utf8');
-var buffer = fs.readFileSync(process.argv[2]);
-console.log(buffer.toString().split('\n').length-1);
+fs.readFile(process.argv[2], 'utf8',function callBack(err,buffer){
+    console.log(buffer.toString().split('\n').length-1);
+});
+// var fs = require('fs')
+// var file = process.argv[2]
+
+// fs.readFile(file, function (err, contents) {
+//     if (err) {
+//     return console.log(err)
+//     }
+//     // fs.readFile(file, 'utf8', callback) can also be used
+//     var lines = contents.toString().split('\n').length - 1
+//     console.log(lines)
+// })
