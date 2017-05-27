4f388523fb5024e09191f8c6eb547c4c0986b413
Martin Madrigal <teenmv92@gmail.com>
2017-05-26 22:19:41 -0600

    Saving baby steps

--- program.js
+++ program.js
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD"); 
+function init(){
+    var inputsArray = process.argv; 
+    var result=0;
+    for(var i = 2; i< inputsArray.length;i++){
+        if(isNumber(inputsArray[i])){
+            result+=Number(inputsArray[i]);
+        }
+    }
+    console.log(result);
+}
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+init();
